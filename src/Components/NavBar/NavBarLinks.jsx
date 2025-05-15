14dcfcf0e62e41a2fd825c5604e6d4a675f0ea21
dinul <dinuluxshan110@gmail.com>
2025-05-15 07:06:44 +0530

    checking mobile view issue

--- src/Components/NavBar/NavBarLinks.jsx
+++ src/Components/NavBar/NavBarLinks.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-scroll';
 
 const NavBarLinks = () => {
   return (
-    <ul className='flex gap-6 text-white font-bold text-center sm:flex-col lg:flex-row sm:absolute lg:relative sm:top-[120%] left-[50%] -translate-x-[50%] lg:text-md sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full py-4'>
+    <ul className='flex flex-col gap-6 text-white font-bold text-center sm:flex-col sm:top-full lg:flex-row sm:absolute sm:left-1/2 lg:relative left-[50%] -translate-x-[50%] lg:text-md sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full py-4 '>
       {AllNavBarArray.map((link, index) => {
         return(
             <li key={index} className='group'>
