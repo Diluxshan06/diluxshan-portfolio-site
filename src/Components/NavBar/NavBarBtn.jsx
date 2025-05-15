3183a93674f0f1f08ac63f7707f4dcd1968b450e
dinul <dinuluxshan110@gmail.com>
2025-05-15 07:41:26 +0530

    checking mobile view issue

--- src/Components/NavBar/NavBarBtn.jsx
+++ src/Components/NavBar/NavBarBtn.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-scroll';
 
 const NavBarBtn = () => {
   return (
-    <div>
-        <Link to='contact' spy={true} smooth={true} offset={-130} duration={500}><button className='px-4 py-2 text-xl font-bold text-white border-cyan border flex items-center gap-1 rounded-full bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyandShadow'>
-            Hire Me
-            <LuArrowDownRight size={20} className="sm:hidden lg:block"/>
-        </button></Link>
-    </div>
+    <button className=" px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
+      <Link spy={true} smooth={true} duration={500} offset={-120} to="contact">
+        Hire Me
+      </Link>
+      <div className="sm:hidden md:block">
+        <LuArrowDownRight />
+      </div>
+    </button>
   )
 }
 
