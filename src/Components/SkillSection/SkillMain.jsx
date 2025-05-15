2687c230b55c0b3676f5e20deff4a3a4ed393b5f
dinul <dinuluxshan110@gmail.com>
2025-05-15 07:27:27 +0530

    checking mobile view issue

--- src/Components/SkillSection/SkillMain.jsx
+++ src/Components/SkillSection/SkillMain.jsx
@@ -9,9 +9,9 @@ import { fadeIn } from '../../framerMotion/varients'
 const SkillMain = () => {
   return (
     <div id='skills'>
-        <div className='max-w-[1200px] px-4 mx-auto min-h-[600px] relative overflow-hidden'>
+        <div className='max-w-[1200px] px-4 mx-auto min-h-[500px] relative overflow-hidden'>
           <motion.div
-          variants={fadeIn('down',0.2)}
+          variants={fadeIn('down',0.1)}
           initial='hidden'
           whileInView='show'
           viewport={{once:false, amount:0.7}}
--- src/Components/SkillSection/SkillText.jsx
+++ src/Components/SkillSection/SkillText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillText = () => {
   return (
-    <div className='flex flex-col items-center mt-[100px]'>
+    <div className='flex flex-col items-center mt-[80px]'>
         <h2 className='text-6xl text-cyan mb-10'>My Skills</h2>
         <p className='text-lg text-center text-white'>
             I not only work with these technologies but excel at
